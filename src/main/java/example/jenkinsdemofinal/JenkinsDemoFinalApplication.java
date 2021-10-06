90c23a9d8ff56656b8873585f62cc0e7759d4268
MadsKolbye <christiane1662@yahoo.dk>
2021-10-06 11:25:52 +0200

    Mit commit

---
+++ src/main/java/example/jenkinsdemofinal/JenkinsDemoFinalApplication.java
@@ -0,0 +1,13 @@
+package example.jenkinsdemofinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsDemoFinalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsDemoFinalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/example/jenkinsdemofinal/JenkinsDemoFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package example.jenkinsdemofinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsDemoFinalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
